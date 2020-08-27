fb47928dc03387173005297940576d3de700a42c
alvaro <alvarovascon2@gmail.com>
2020-08-27 18:25:27 -0300

    exercicios 6 a 8

--- exercises/4_1/ex2maior.js
+++
@@ -1,8 +0,0 @@
-const a = 14;
-const b = 9;
-
-if(a>b){
-  console.log(a);
-}else{
-  console.log(b);
-}
---
+++ exercises/4_1/ex8_par.js
@@ -0,0 +1,9 @@
+let a = 8;
+let b = 9;
+let c = 15;
+
+if((a%2)==0 || (b%2)==0 || (c%2)==0 ){
+  console.log(true);
+} else {
+  console.log(false);
+}
