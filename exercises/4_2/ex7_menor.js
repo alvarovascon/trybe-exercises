9fbd6d55f394beb8880c123d840c9675d246e1de
alvaro <alvarovascon2@gmail.com>
2020-08-28 17:02:03 -0300

    exercícios 8 e 9

--- exercises/4_2/ex7_menor.js
+++ exercises/4_2/ex7_menor.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let menor = numbers[1];
 
-for (let i=0;i < numbers.length; i++){
+for (let i=0;i < numbers.length; i += 1){
   if (numbers[i] < menor){
     menor = numbers[i];
   }
