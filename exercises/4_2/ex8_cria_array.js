0b272912eb3960a60aa3ca959378691bec9fec34
alvaro <alvarovascon2@gmail.com>
2020-08-28 19:15:42 -0300

    Alterações codeClimate <3

--- exercises/4_2/ex8_cria_array.js
+++ exercises/4_2/ex8_cria_array.js
@@ -1,6 +1,6 @@
 let lista = [];
 
-for (let i = 0; i < 25; i += 1){
+for (let i = 0; i < 25; i += 1) {
   lista[i]= i + 1;
 }
 
--- exercises/4_2/ex9_divide_array.js
+++ exercises/4_2/ex9_divide_array.js
@@ -1,7 +1,7 @@
 let lista = [];
 let listaDividida = [];
 
-for (let i = 0; i < 25; i += 1){
+for (let i = 0; i < 25; i += 1) {
   lista[i]= i + 1;
   listaDividida[i] = lista[i]/2;
 }
