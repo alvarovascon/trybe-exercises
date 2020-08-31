c019795e0a285afa6f302bf771a6fd5da3d6da09
alvaro <alvarovascon2@gmail.com>
2020-08-31 15:23:34 -0300

    exercicio 3 - retirando um loop inútil

--- exercises/4_3/ex3_imprime_triangulo_invertido.js
+++ exercises/4_3/ex3_imprime_triangulo_invertido.js
@@ -2,7 +2,7 @@ let n = 6;
 let string = "";
 
 for (i = 0; i < n; i += 1) {
-  for (j = n; j >= 0; j -= 1) {
+  for (j = n - 1; j >= 0; j -= 1) {
     if (j > i){
       string += " ";
     } else {
