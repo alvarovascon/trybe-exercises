e3d551b4cfb1dab2b7217e028d435840c0ff4d2d
alvaro <alvarovascon2@gmail.com>
2020-09-01 18:19:37 -0300

    alteraçoes em 2.2 e terminando 2.3 e 2.4

--- exercises/4_4/ex2_2_indice_maior.js
+++ exercises/4_4/ex2_2_indice_maior.js
@@ -1,9 +1,9 @@
 let array = [2, 3, 6, 7, 10, 1];
 
 function getBiggerIndex(arrayNumbers) {
-  let ceil = 0;
+  let ceil = arrayNumbers[0];
   let ceilIndex;
-  for (let i = 0;i < arrayNumbers.length; i += 1) {
+  for (let i = 0;i < arrayNumbers.length; i += 1) { // poderia ser for(let i in arraynumbers)
     if (arrayNumbers[i] > ceil) {
       ceil = arrayNumbers[i];
       ceilIndex = i;
