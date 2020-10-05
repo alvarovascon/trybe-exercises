1856f74d4ec60483eceb106cac60f43f36ab631a
alvaro <alvarovascon2@gmail.com>
2020-10-05 15:47:50 -0300

    alteração em addOne

--- Introduction - Web Development/bloco_7/7_3/addOne.js
+++ Introduction - Web Development/bloco_7/7_3/addOne.js
@@ -14,5 +14,5 @@ const expected = [32, 58, 13, 6];
 const output = addOne(myArray);
 
 assert.strictEqual(typeof addOne, 'function');
-assert.deepEqual(output, expected);
-assert.deepEqual(myArray, unchanged);
+assert.deepStrictEqual(output, expected);
+assert.deepStrictEqual(myArray, unchanged);
