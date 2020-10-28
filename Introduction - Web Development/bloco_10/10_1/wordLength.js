903b54beed2886b8a20efcbea2cdb3114908b6b9
alvaro <alvarovascon2@gmail.com>
2020-10-28 18:35:38 -0300

    teste wordlength

--- Introduction - Web Development/bloco_10/10_1/wordLength.js
+++ Introduction - Web Development/bloco_10/10_1/wordLength.js
@@ -1,4 +1,3 @@
-const assert = require('assert');
 // escreva a função wordLengths aqui
 function wordLengths (words) {
   result = [];
@@ -8,9 +7,4 @@ function wordLengths (words) {
   return result;
 }
 
-const words = ['sun', 'potato', 'roundabout', 'pizza'];
-const expected = [3, 6, 10, 5];
-
-assert.strictEqual(typeof wordLengths, 'function');
-const output = wordLengths(words);
-assert.deepEqual(output, expected);
+module.exports = wordLengths;
