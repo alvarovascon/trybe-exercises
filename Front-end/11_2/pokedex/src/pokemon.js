17dbe6b12e98c5a1a4f945d6ef3e5e71489e4439
alvaro <alvarovascon2@gmail.com>
2020-11-06 19:34:04 -0300

    pokedex

---
+++ Front-end/11_2/pokedex/src/pokemon.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class Pokemon extends React.Component {
+  render() {
+    const { name, type, averageWeight, image, moreInfo } = this.props.pokemon;
+    return (
+      <div>
+        <h4>{name}</h4>
+        <p>type: {type}</p>
+        <p>weigth: {averageWeight.value} {averageWeight.measurementUnit}</p>
+        <img src={image}></img>
+        <p>additional info: {moreInfo}</p>
+      </div>
+    )
+  }
+}
+
+export default Pokemon;
