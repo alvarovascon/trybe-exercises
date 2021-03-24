3741df91eaab934bd64bda68a719a1be6b6918be
alvarovascon <alvarovascon2@gmail.com>
2021-03-24 16:57:21 -0300

    usando inquirer

--- Back-end/bloco_26/26_1/imc_package/index.js
+++ Back-end/bloco_26/26_1/imc_package/index.js
@@ -1,30 +1,38 @@
 const readlineSync = require('readline-sync');
 
-const peso = readlineSync.question('Qual seu peso? ');
+const inquirer = require('inquirer');
 
-const altura = readlineSync.question('Qual sua altura? ');
+function calculaIMC(answers) {
+  const imc = answers.peso / (answers.altura ** 2);
 
-const imc = peso / (altura ^ 2);
+if(imc < 18.5) {
+  console.log(`Seu IMC é ${imc.toFixed(2)}, você está abaixo do peso (magreza)`);
+}
+if(imc >= 18.5 && imc < 24.9) {
+  console.log(`Seu IMC é ${imc.toFixed(2)}, você está no peso normal`);
+}
+if(imc >= 25 && imc < 29.9) {
+  console.log(`Seu IMC é ${imc.toFixed(2)}, você está acima do peso (sobrepeso)`);
+}
+if(imc >= 30 && imc < 34.9) {
+  console.log(`Seu IMC é ${imc.toFixed(2)}, você está na faixa de obesidade grau I`);
+}
+if(imc >= 35 && imc < 39.9) {
+  console.log(`Seu IMC é ${imc.toFixed(2)}, você está na faixa de obesidade grau II`);
+}
+if(imc >= 40) {
+  console.log(`Seu IMC é ${imc.toFixed(2)}, você está na faixa de obesidade grau III ou IV`);
+}
 
-const showSituation = (imc) => {
-  if(imc < 18.5) {
-    console.log(`Seu IMC é ${imc.toFixed(2)}, você está abaixo do peso (magreza)`);
-  }
-  if(imc >= 18.5 && imc < 24.9) {
-    console.log(`Seu IMC é ${imc.toFixed(2)}, você está no peso normal`);
-  }
-  if(imc >= 25 && imc < 29.9) {
-    console.log(`Seu IMC é ${imc.toFixed(2)}, você está acima do peso (sobrepeso)`);
-  }
-  if(imc >= 30 && imc < 34.9) {
-    console.log(`Seu IMC é ${imc.toFixed(2)}, você está na faixa de obesidade grau I`);
-  }
-  if(imc >= 35 && imc < 39.9) {
-    console.log(`Seu IMC é ${imc.toFixed(2)}, você está na faixa de obesidade grau II`);
-  }
-  if(imc >= 40) {
-    console.log(`Seu IMC é ${imc.toFixed(2)}, você está na faixa de obesidade grau III ou IV`);
-  }
-};
+}
 
-showSituation(imc);
+
+inquirer
+  .prompt([
+    { name: 'peso', type: 'number', message: 'Qual o seu peso?' },
+    { name: 'altura', type: 'number', message: 'Qual sua altura?' },
+  ])
+    .then(answers => {
+      calculaIMC(answers);
+    })
+      .catch();
