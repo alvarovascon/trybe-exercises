a9039aa2ec8f83741723f028128bc5499ca43195
alvarovascon <alvarovascon2@gmail.com>
2021-03-24 16:02:31 -0300

    exibindo situaçao

--- Back-end/bloco_26/26_1/imc_package/index.js
+++ Back-end/bloco_26/26_1/imc_package/index.js
@@ -4,6 +4,27 @@ const peso = readlineSync.question('Qual seu peso? ');
 
 const altura = readlineSync.question('Qual sua altura? ');
 
-const imc = (peso / (altura ^ 2)).toFixed(2);
+const imc = peso / (altura ^ 2);
 
-console.log(`imc: ${imc}` );
+const showSituation = (imc) => {
+  if(imc < 18.5) {
+    console.log(`Seu IMC é ${imc.toFixed(2)}, você está abaixo do peso (magreza)`);
+  }
+  if(imc >= 18.5 && imc < 24.9) {
+    console.log(`Seu IMC é ${imc.toFixed(2)}, você está no peso normal`);
+  }
+  if(imc >= 25 && imc < 29.9) {
+    console.log(`Seu IMC é ${imc.toFixed(2)}, você está acima do peso (sobrepeso)`);
+  }
+  if(imc >= 30 && imc < 34.9) {
+    console.log(`Seu IMC é ${imc.toFixed(2)}, você está na faixa de obesidade grau I`);
+  }
+  if(imc >= 35 && imc < 39.9) {
+    console.log(`Seu IMC é ${imc.toFixed(2)}, você está na faixa de obesidade grau II`);
+  }
+  if(imc >= 40) {
+    console.log(`Seu IMC é ${imc.toFixed(2)}, você está na faixa de obesidade grau III ou IV`);
+  }
+};
+
+showSituation(imc);
